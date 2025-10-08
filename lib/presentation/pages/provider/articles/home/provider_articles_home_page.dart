f1ffb3082cf1349f4c308e591bc716fe60d6a621
alexrudas <alexrudas@hotmail.com>
2025-10-07 20:18:46 -0500

    fix(provider): bind home controllers in pages via Get.put to avoid 'Controller not found' exceptions

--- lib/presentation/pages/provider/articles/home/provider_articles_home_page.dart
+++ lib/presentation/pages/provider/articles/home/provider_articles_home_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../../widgets/loading_state.dart';
-import '../../../../widgets/error_state.dart';
-import '../../../../widgets/empty_state.dart';
-import '../../../../controllers/provider/articles/home/provider_articles_home_controller.dart';
 
-class ProviderArticlesHomePage extends GetView<ProviderArticlesHomeController> {
+import '../../../../controllers/provider/articles/home/provider_articles_home_controller.dart';
+import '../../../../widgets/empty_state.dart';
+import '../../../../widgets/error_state.dart';
+import '../../../../widgets/loading_state.dart';
+
+class ProviderArticlesHomePage extends StatelessWidget {
   const ProviderArticlesHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ProviderArticlesHomeController());
     return Obx(() {
       if (controller.loading.value) return const LoadingState();
-      if (controller.error.value != null) return ErrorState(message: controller.error.value!);
+      if (controller.error.value != null)
+        return ErrorState(message: controller.error.value!);
       return ListView(
         padding: const EdgeInsets.all(16),
         children: const [
--- lib/presentation/pages/provider/services/home/provider_services_home_page.dart
+++ lib/presentation/pages/provider/services/home/provider_services_home_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../../widgets/loading_state.dart';
-import '../../../../widgets/error_state.dart';
-import '../../../../widgets/empty_state.dart';
-import '../../../../controllers/provider/services/home/provider_services_home_controller.dart';
 
-class ProviderServicesHomePage extends GetView<ProviderServicesHomeController> {
+import '../../../../controllers/provider/services/home/provider_services_home_controller.dart';
+import '../../../../widgets/empty_state.dart';
+import '../../../../widgets/error_state.dart';
+import '../../../../widgets/loading_state.dart';
+
+class ProviderServicesHomePage extends StatelessWidget {
   const ProviderServicesHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ProviderServicesHomeController());
     return Obx(() {
       if (controller.loading.value) return const LoadingState();
-      if (controller.error.value != null) return ErrorState(message: controller.error.value!);
+      if (controller.error.value != null)
+        return ErrorState(message: controller.error.value!);
       return ListView(
         padding: const EdgeInsets.all(16),
         children: const [
