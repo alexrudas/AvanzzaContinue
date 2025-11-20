8f397b48ffab451f0e10e8559f4097c15f232420
alexrudas <alexrudas@hotmail.com>
2025-11-20 18:09:15 -0500

    Update google-services.json with google auth and add UserModelSchema to  allIsarSchemas

--- lib/core/db/isar_schemas.dart
+++ lib/core/db/isar_schemas.dart
@@ -8,6 +8,7 @@ import 'package:avanzza/data/models/maintenance/incidencia_model.dart';
 import 'package:avanzza/data/models/org/organization_model.dart';
 import 'package:avanzza/data/models/settings/theme_pref_model.dart';
 import 'package:avanzza/data/models/user/membership_model.dart';
+import 'package:avanzza/data/models/user/user_model.dart';
 import 'package:avanzza/data/models/workspace/workspace_lite_model.dart';
 import 'package:avanzza/data/models/workspace/workspace_state_model.dart';
 
@@ -24,4 +25,5 @@ final allIsarSchemas = [
   ThemePreferenceModelSchema,
   WorkspaceLiteModelSchema,
   WorkspaceStateModelSchema,
+  UserModelSchema
 ];
